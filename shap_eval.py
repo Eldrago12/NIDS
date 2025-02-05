63358221185ba6522527a5e5db24c4fb51afe710
Sirshak Dolai <116832350+Eldrago12@users.noreply.github.com>
2025-02-06 02:28:41 +0530

    Updated SHAP code

--- shap_eval.py
+++ shap_eval.py
@@ -33,26 +33,22 @@ def capture_print(func):
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 logging.info(f"Using device: {device}")
 
-# Load and preprocess data
 df = pd.read_csv('/storage/research/data/nids/NSL-KDD/KDDTrain+.csv')
 categorical_columns = ['protocol_type', 'service', 'flag']
 le = LabelEncoder()
 for col in categorical_columns:
     df[col] = le.fit_transform(df[col])
 
-# Split features and labels
 X = df.drop(columns=['xAttack'])
 y = df['xAttack']
 le_target = LabelEncoder()
 y = le_target.fit_transform(y)
 
-# Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 scaler = StandardScaler()
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
 
-# Apply PCA
 pca = PCA(n_components=0.95)
 X_train_pca = pca.fit_transform(X_train_scaled)
 X_test_pca = pca.transform(X_test_scaled)
